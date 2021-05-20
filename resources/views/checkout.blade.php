e148c329a34fcdf7ddbf322aeb635777cee73bd1
Victor Okonkwo <emmyvic98@gmail.com>
2021-05-20 08:31:46 +0100

    Checkout Page Template

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -11,8 +11,8 @@
               <div class="col-lg-6 text-lg-right">
                 <nav aria-label="breadcrumb">
                   <ol class="breadcrumb justify-content-lg-end mb-0 px-0">
-                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                    <li class="breadcrumb-item"><a href="cart.html">Cart</a></li>
+                    <li class="breadcrumb-item"><a href="/">Home</a></li>
+                    <li class="breadcrumb-item"><a href="/cart">Cart</a></li>
                     <li class="breadcrumb-item active" aria-current="page">Checkout</li>
                   </ol>
                 </nav>
