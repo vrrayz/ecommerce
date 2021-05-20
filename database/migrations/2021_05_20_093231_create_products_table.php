320cce710dd2e45b3fe89f8428de7638c6030851
Victor Okonkwo <emmyvic98@gmail.com>
2021-05-20 11:17:55 +0100

    Model Migrations

---
+++ database/migrations/2021_05_20_093231_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('price',10);
+            $table->text('description');
+            $table->integer('quantity')->default(0);
+            $table->string('tags');
+            $table->timestamps();
+            $table->foreignId('sub_category_id')->references('id')->on('sub_categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
